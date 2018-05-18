8bd9c4f2ca70afaa53dcd10fccfa67426b75b4cc
Ubuntu <ubuntu@ip-172-31-41-237.us-east-2.compute.internal>
2018-05-18 08:24:52 +0000

    10th

--- main.py
+++ main.py
@@ -147,8 +147,8 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
         batch_count=0
         for image, label in get_batches_fn(batch_size):
             _, loss = sess.run([train_op, cross_entropy_loss],
-                               feed_dict={input_image: image, correct_label: label, keep_prob: 0.4,
-                                          learning_rate: 0.001})
+                               feed_dict={input_image: image, correct_label: label, keep_prob: 0.5,
+                                          learning_rate: 0.0005})
             batch_count+=1
             tot_loss+=loss
             print("epoch no",i,"batch_number",batch_count,"loss = {:.3f}".format(loss))
@@ -183,7 +183,7 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
-        epochs = 12
+        epochs = 20
         batch_size = 5
 
         # TF placeholders
