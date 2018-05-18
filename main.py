e57f4ae26ac2b02126a7d1aed056a68efd64bd82
Ubuntu <ubuntu@ip-172-31-7-57.us-east-2.compute.internal>
2018-05-18 02:47:42 +0000

    5th attempt

--- main.py
+++ main.py
@@ -150,10 +150,10 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
                                feed_dict={input_image: image, correct_label: label, keep_prob: 0.6,
                                           learning_rate: 0.0005})
             batch_count+=1
-            total_loss+=loss
-            print("=")
-         print()
-         print("Avg loss: = {:.3f}".format(tot_loss/batch_count))
+            tot_loss+=loss
+            print("epoch no",i,"batch_number",batch_count,"loss = {:.3f}".format(loss))
+        print()
+        print("Avg loss: = {:.3f}".format(tot_loss/batch_count))
         
 
 tests.test_train_nn(train_nn)
