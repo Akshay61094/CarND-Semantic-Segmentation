f22be4b49052f4dac4300c4e64680e1efaff5449
Ubuntu <ubuntu@ip-172-31-41-237.us-east-2.compute.internal>
2018-05-18 06:35:49 +0000

    9th attempt

--- main.py
+++ main.py
@@ -147,8 +147,8 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
         batch_count=0
         for image, label in get_batches_fn(batch_size):
             _, loss = sess.run([train_op, cross_entropy_loss],
-                               feed_dict={input_image: image, correct_label: label, keep_prob: 0.5,
-                                          learning_rate: 0.0005})
+                               feed_dict={input_image: image, correct_label: label, keep_prob: 0.4,
+                                          learning_rate: 0.001})
             batch_count+=1
             tot_loss+=loss
             print("epoch no",i,"batch_number",batch_count,"loss = {:.3f}".format(loss))
@@ -184,7 +184,7 @@ def run():
 
         # TODO: Build NN using load_vgg, layers, and optimize function
         epochs = 12
-        batch_size = 4
+        batch_size = 5
 
         # TF placeholders
         correct_label = tf.placeholder(tf.int32, [None, None, None, num_classes], name='correct_label')
